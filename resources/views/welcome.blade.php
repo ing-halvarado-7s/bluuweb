b09c41db6b09a2f155114195d415cef01981fc00
ing.halvarado.7s <ing.halvarado.7s@gmail.com>
2020-03-08 16:09:05 -0430

    Agregar tabla para los datos de la base de datos en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,36 @@
   <body>
     <div class="container my-4">
         <h1 class="header-4">Notas</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Descripción</th>
+                <th scope="col">Funciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
     </div>
 
     <!-- Optional JavaScript -->
